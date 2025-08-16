3dd31679f9970e5d7c9c1bb9f7d2d80ee4e44721
somin0567 <koala0567@gmail.com>
2025-08-16 15:52:55 +0900

    feat: 버튼 컴포넌트 추가(그룹장 화면 기준)

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,12 +1,30 @@
+import React from 'react'
+import TaskHistoryButton from '../features/tasks/components/TaskHistoryButton'
 import TaskTable from '../features/tasks/components/TaskTable'
+import TaskRandomButton from '../features/tasks/components/TaskRandomButton'
+import TaskExchangeButton from '../features/tasks/components/TaskExchangeButton'
+import CheckRepeat from '../features/tasks/components/CheckRepeat'
+/* 그룹장 화면 기준 ui */
+const TasksPage: React.FC = () => {
+  const [repeat, setRepeat] = React.useState(true)
 
-const Tasks = () => {
   return (
-    <div className="space-y-6">
-      <h1 className="text-3xl font-bold text-primary">주간 업무표</h1>
+    <div className="p-4 space-y-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl font-bold text-primary">주간 업무표</h1>
+        <TaskHistoryButton onClick={() => {}} />
+      </div>
       <TaskTable />
+
+      <div className="flex flex-col items-center space-y-4 mt-2">
+        <div className="flex space-x-2">
+          <TaskRandomButton onClick={() => {}} />
+          <TaskExchangeButton onClick={() => {}} />
+        </div>
+        <CheckRepeat checked={repeat} onChange={(e) => setRepeat(e.target.checked)} />
+      </div>
     </div>
   )
 }
 
-export default Tasks
+export default TasksPage
