ae2e22db1dfd5edccc6acb7e3e84fc0e8bd3b8d8
somin0567 <koala0567@gmail.com>
2025-08-16 18:25:56 +0900

    feat: bootstrap-icon으로 수정

--- src/features/tasks/components/TaskTable.tsx
+++ src/features/tasks/components/TaskTable.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import DaySelectModal from './DaySelectModal'
+import { ChevronRight, PlusCircleFill } from 'react-bootstrap-icons'
 
 const days = ['일', '월', '화', '수', '목', '금', '토']
 const initialTasks = ['청소', '분리수거', '설거지']
@@ -32,7 +33,6 @@ const TaskTable: React.FC = () => {
             ))}
           </tr>
         </thead>
-
         <tbody>
           {tasks.map((task, rowIdx) => (
             <tr key={rowIdx}>
@@ -41,27 +41,17 @@ const TaskTable: React.FC = () => {
                 style={{ borderRight: '1px solid #D1D5DB' }}
               >
                 <span>{task}</span>
-
                 <div className="relative flex items-center">
                   <button
                     type="button"
                     className="text-gray-400 focus:text-black z-20 -ml-4"
                     onClick={() => toggleModal(rowIdx)}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      viewBox="0 0 16 16"
+                    <ChevronRight
+                      size={16}
                       className={openModal === rowIdx ? 'scale-x-[-1] transition-transform' : ''}
                       style={openModal === rowIdx ? { transform: 'scaleX(-1)' } : undefined}
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
-                      />
-                    </svg>
+                    />
                   </button>
                   {openModal === rowIdx && (
                     <DaySelectModal
@@ -92,16 +82,7 @@ const TaskTable: React.FC = () => {
                   className="bg-transparent border-none p-0 m-0"
                   style={{ cursor: 'pointer', lineHeight: 1 }}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    fill="gray"
-                    className="bi bi-plus-circle-fill"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
-                  </svg>
+                  <PlusCircleFill size={18} color="gray" />
                 </button>
               </div>
             </td>
