6599fa1f3e53eb996d86862f75196b4e93de85c3
somin0567 <koala0567@gmail.com>
2025-08-18 23:13:37 +0900

    feat: 참여 그룹원 리스트 추가

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -4,7 +4,15 @@ import TaskTable from '../features/tasks/components/TaskTable'
 import TaskRandomButton from '../features/tasks/components/TaskRandomButton'
 import TaskExchangeButton from '../features/tasks/components/TaskExchangeButton'
 import CheckRepeat from '../features/tasks/components/CheckRepeat'
+import GroupMemberList from '../features/tasks/components/GroupMemberList'
 /* 그룹장 화면 기준 ui */
+
+const members = [
+  { name: '그룹원1', avatarUrl: '/' },
+  { name: '그룹원2', avatarUrl: '/' },
+  { name: '그룹원3', avatarUrl: '/' },
+]
+
 const TasksPage: React.FC = () => {
   const [repeat, setRepeat] = React.useState(true)
 
@@ -15,7 +23,6 @@ const TasksPage: React.FC = () => {
         <TaskHistoryButton onClick={() => {}} />
       </div>
       <TaskTable />
-
       <div className="flex flex-col items-center space-y-4 mt-2">
         <div className="flex space-x-2">
           <TaskRandomButton onClick={() => {}} />
@@ -23,6 +30,8 @@ const TasksPage: React.FC = () => {
         </div>
         <CheckRepeat checked={repeat} onChange={(e) => setRepeat(e.target.checked)} />
       </div>
+      <div className="font-bold text-md mt-4 text-primary">참여 그룹원</div>
+      <GroupMemberList members={members} />
     </div>
   )
 }
