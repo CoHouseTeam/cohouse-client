61119fa99ac169d08036eaf64802d1f277be6358
somin0567 <koala0567@gmail.com>
2025-08-29 01:38:01 +0900

    feat: 그룹초대 모달 ui 수정

--- src/features/mainpage/components/GroupInviteModal.tsx
+++ src/features/mainpage/components/GroupInviteModal.tsx
@@ -9,37 +9,45 @@ const GroupInviteModal: React.FC<InviteModalProps> = ({
   onDecline,
 }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black/40">
-      <div className="relative bg-white rounded-lg w-[90%] max-w-sm mx-auto px-6 py-8 shadow-lg">
-        <button
-          className="absolute right-3 top-3"
-          onClick={onClose}
-          aria-label="닫기"
-          type="button"
-        >
-          <XCircleFill className="text-2xl text-gray-400" />
-        </button>
-        <div className="flex flex-col items-center justify-center">
-          <p className="text-[18px] font-medium mt-4 mb-8 text-center">
-            {groupName} 그룹에 초대 되었습니다.
-          </p>
-          <div className="flex justify-center space-x-3 mt-2 w-full">
-            <button
-              className="btn bg-gray-200 text-black text-[16px] w-[40%] rounded-lg hover:bg-gray-300"
-              onClick={onDecline}
-            >
-              거절
-            </button>
-            <button
-              className="btn bg-black text-white text-[16px] w-[40%] rounded-lg hover:bg-gray-700"
-              onClick={onAccept}
-            >
-              승인
-            </button>
+    <dialog open className="modal">
+      <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div className="relative bg-white rounded-lg w-[90%] max-w-sm mx-auto px-6 py-8 shadow-lg">
+          <button
+            className="absolute right-3 top-3"
+            onClick={onClose}
+            aria-label="닫기"
+            type="button"
+          >
+            <XCircleFill className="text-xl text-gray-400" />
+          </button>
+
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-[18px] font-medium mt-4 mb-8 text-center">
+              {groupName} 그룹에 초대 되었습니다.
+            </p>
+            <div className="flex justify-center space-x-3 mt-2 w-full">
+              <button
+                className="btn bg-gray-200 text-black text-[16px] w-[40%] rounded-lg hover:bg-gray-300"
+                onClick={onDecline}
+              >
+                거절
+              </button>
+              <button
+                className="btn bg-black text-white text-[16px] w-[40%] rounded-lg hover:bg-gray-700"
+                onClick={onAccept}
+              >
+                승인
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <form
+        method="dialog"
+        className="modal-backdrop fixed inset-0 bg-black/40"
+        onClick={onClose}
+      ></form>
+    </dialog>
   )
 }
 
